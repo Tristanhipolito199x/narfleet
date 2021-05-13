eb30688993d9aede5b258c6f821c92d6f57638d6
Tristan Hipolito <tristan.hipolito199x@gmail.com>
2021-05-13 22:38:43 +0800

    Update Gallery.js

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,7 +1,7 @@
 import GalleryBanner from "./gallery-banner/GalleryBanner"
 import ExploreNext4 from './explore-next4/ExploreNext4'
 import CarouselGallery from './carousel/Carousel'
-import { useState } from 'react'
+
 
 
 const Gallery = () => {
